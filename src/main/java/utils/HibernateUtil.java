4810fd850145596b2852bd5ee8d0feab1ca70544
Artem Nekipelov <leet5@mail.ru>
2018-10-22 18:30:19 +0300

    Utils and hibernate.cfg added

---
+++ src/main/java/utils/HibernateUtil.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable e) {
+            throw new ExceptionInInitializerError();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdownSF() {
+        getSessionFactory().close();
+    }
+}
